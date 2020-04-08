c4b52e1b1aac52e2f2c5495450b8573d5dc54485
Glitch (timestamp-service-lb) <none>
2020-04-08 18:27:48 +0000

    🎓🌮 Updated with Glitch

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-// server.js
-// where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// Enables style.css file
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+// Loads index.html file when you enter app
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Timestamp service
+//User story 1 - The API endpoint is GET [project_url]/api/timestamp/:date_string?
 app.get("/api/timestamp/:dateString?", (req, res) => { //:dateString? - The ? means that the request parameter is optional - allows the case that no date is input
   let dateString = req.params.dateString;
   let date;
   
+  //User story 2 - A date string is valid if can be successfully parsed by new Date(date_string).
+  //Note that the unix timestamp needs to be an integer (not a string) specifying milliseconds.
+  //In our test we will use date strings compliant with ISO-8601 (e.g. "2016-11-20") because this will ensure an UTC timestamp.
   if (dateString == null) {
-    //If the date string is empty it should be equivalent to trigger new Date(), i.e. the service uses the current timestamp
-    date = new Date();
+    date = new Date(); //User story 3 - If the date string is empty it should be equivalent to trigger new Date(), i.e. the service uses the current timestamp.
   } else {
     if (isNaN(dateString)) { // if the date string is a string, then we just use the regular Date function
       date = new Date(dateString);
@@ -35,17 +33,17 @@ app.get("/api/timestamp/:dateString?", (req, res) => { //:dateString? - The ? me
   };
 
   if (date == "Invalid Date") {
-    //If the date string is invalid the api returns a JSON having the structure {"error" : "Invalid Date" }. 
-    res.json({"error": "Invalid Date"});
+    res.json({"error": "Invalid Date"}); //User story 5 - If the date string is invalid the api returns a JSON having the structure {"error" : "Invalid Date" }
   } else {
-    //If the date string is valid the api returns a JSON having the structure {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+    //User story 4 - If the date string is valid the api returns a JSON having the structure
+    //{"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+    //e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}
     res.json({"unix": date.getTime(), "utc": date.toUTCString()});
   }
 });
 
-
 // listen for requests
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
